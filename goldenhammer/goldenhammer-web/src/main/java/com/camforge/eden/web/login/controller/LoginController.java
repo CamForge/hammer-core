08eb650fe010fe122fdc79ef7ac69808d9717542
CharlesMowers <Charles.Mowers@suny.edu>
2015-01-21 14:03:02 -0500

    Signed-off-by: CharlesMowers <Charles.Mowers@suny.edu>

---
+++ goldenhammer/goldenhammer-web/src/main/java/com/camforge/eden/web/login/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.camforge.eden.web.login.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/** 
+ * @author:  developer
+ * @Created: Jan 21, 2015 at 2:11:22 PM
+ *
+ * @Last_Updated_By: $Author$
+ * @Last_Updated_On: $Date$
+ * @Revision $Rev$
+ * @Project: goldenhammer-web
+ * @Location: $URL$
+ *
+ **/
+@ManagedBean
+@SessionScoped
+public class LoginController {
+
+}
